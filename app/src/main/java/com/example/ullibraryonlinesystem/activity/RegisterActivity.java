dbb35534c8873b48c320f682445234e7939ebc4b
Siayyy <131888565+Siayyy@users.noreply.github.com>
2024-05-12 16:05:57 +0100

    optimized layout, add date of birth function when register

--- app/src/main/java/com/example/ullibraryonlinesystem/activity/RegisterActivity.java
+++ app/src/main/java/com/example/ullibraryonlinesystem/activity/RegisterActivity.java
@@ -2,28 +2,34 @@ package com.example.ullibraryonlinesystem.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.ullibraryonlinesystem.R;
 import com.example.ullibraryonlinesystem.db.DBHelper;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.common.api.ApiException;
-import com.google.firebase.auth.AuthResult;
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnCompleteListener;
 
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -43,79 +49,91 @@ public class RegisterActivity extends AppCompatActivity {
 
         // Initialize Firebase Auth and configure Google Sign-In
         mAuth = FirebaseAuth.getInstance();
-        // Configure Google Sign-In
+        configureGoogleSignIn();
+
+        dbHelper = new DBHelper(this);
+        initializeUI();
+    }
+
+    private void configureGoogleSignIn() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        Button googleRegButton = findViewById(R.id.btnGoogleReg);
-        googleRegButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
+        findViewById(R.id.btnGoogleReg).setOnClickListener(view -> signIn());
+    }
 
-
-        dbHelper = new DBHelper(this);
+    private void initializeUI() {
         emailEditText = findViewById(R.id.registerEmail);
         passwordEditText = findViewById(R.id.registerPassword);
         nameEditText = findViewById(R.id.registerName);
-        majorEditText = findViewById(R.id.registerMajor);
+        majorEditText = findViewById(R.id.registerBirth);
         registerButton = findViewById(R.id.btnRegisterUser);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-                String name = nameEditText.getText().toString().trim();
-                String major = majorEditText.getText().toString().trim();
+        registerButton.setOnClickListener(view -> registerUser());
 
-                // 检查是否有未填写的项
-                if (email.isEmpty() || password.isEmpty() || name.isEmpty() || major.isEmpty()) {
-                    Toast.makeText(RegisterActivity.this, "There are unfilled options, please complete them.", Toast.LENGTH_SHORT).show();
-                } else {
-                    // 将数据插入数据库
-                    dbHelper.addUser(email, password, name, major); // 你需要在 DBHelper 中实现这个方法
-                    // 注册成功后返回登录页面
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish(); // 结束当前的 Activity
-                }
-            }
-        });
+        majorEditText.setFocusable(false);
+        majorEditText.setOnClickListener(view -> showDatePickerDialog());
     }
+
+    private void registerUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+        String name = nameEditText.getText().toString().trim();
+        String major = majorEditText.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty() || name.isEmpty() || major.isEmpty()) {
+            Toast.makeText(this, "There are unfilled options, please complete them.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        dbHelper.addUser(email, password, name, major); // Assume implementation
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 9001); // Use a constant for request code
     }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 9001) { // Same request code as above
+        if (requestCode == 9001) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                Toast.makeText(RegisterActivity.this, "Google sign in failed: " + e.getStatusCode(), Toast.LENGTH_LONG).show();
+                Log.e("Google Sign-In Error", "StatusCode: " + e.getStatusCode() + " - " + e.getMessage());
+                Toast.makeText(this, "Google sign in failed: " + e.getStatusCode(), Toast.LENGTH_LONG).show();
             }
         }
+    }
+    private void showDatePickerDialog() {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year1, monthOfYear, dayOfMonth) ->
+                majorEditText.setText(String.format(Locale.getDefault(), "%d-%02d-%02d", year1, monthOfYear + 1, dayOfMonth)),
+                year, month, day);
+
+        datePickerDialog.show();
     }
     private void firebaseAuthWithGoogle(String idToken) {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
         mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            // Optionally, handle the new user information or navigate to another activity
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(RegisterActivity.this, UserCenterActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
